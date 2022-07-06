460a7c08aa42830d04173ffd3a29c1ba3e212c21
Ian Christiansen <ianc.songdog@gmail.com>
2022-07-06 14:20:37 -0700

    changed animation to typewriter effect

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,10 +5,11 @@ export default function Home() {
   return (
     <div className="home-container">
       <span className="title">
-        <h1>
-          hi, i'm <strong>ian</strong>
-          <p className="dot bouncing">.</p>
-        </h1>
+        <span className="typewriter">
+          <h1>
+            hi, i'm <strong>ian</strong>.
+          </h1>
+        </span>
       </span>
       <span className="illustration">
         <img src={Illustration} />
