5e5fbbaadffb20231c0e6902f293627e96493e34
Ian Christiansen <ianc.songdog@gmail.com>
2022-07-06 11:23:52 -0700

    added illustration file, minimal css

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Illustration from '../../../public/assets/illustration.png';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div>
+      <img src={Illustration} />
+    </div>
+  );
+}
