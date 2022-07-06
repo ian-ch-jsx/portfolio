8d7aa715f965cc58d09ffac7a1d16638ac5c563e
Ian Christiansen <ianc.songdog@gmail.com>
2022-07-06 12:02:25 -0700

    fixed webpack configs

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,8 +3,13 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <div>
-      <img src={Illustration} />
+    <div className="home-container">
+      <span className="title">
+        <h1>Hi, I'm Ian</h1>
+      </span>
+      <span className="illustration">
+        <img src={Illustration} />
+      </span>
     </div>
   );
 }
