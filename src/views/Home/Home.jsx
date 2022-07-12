4c9369030e985661389832e5fde4212488a10654
Ian Christiansen <ianc.songdog@gmail.com>
2022-07-12 11:01:16 -0700

    styling

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,11 @@
 import FadeIn from 'react-fade-in';
-import { Zoom } from 'react-reveal';
+import { Fade } from 'react-reveal';
 import Illustration from '../../../public/assets/illustration.png';
 import SelfCareBear from '../../../public/assets/SELFCAREBEAR.png';
 import DBD from '../../../public/assets/DBD.png';
 import GotchiBB from '../../../public/assets/GOTCHIBB.png';
+import GitHub from '../../../public/assets/github.png';
+import LinkedIn from '../../../public/assets/linkedin.png';
 import './Home.css';
 
 export default function Home() {
@@ -23,9 +25,13 @@ export default function Home() {
             <section className="description">
               <p>I'm a software developer from reno, nv.</p>
             </section>
+            <section className="description">
+              <img src={GitHub} />
+              <img src={LinkedIn} />
+            </section>
           </section>
         </section>
-        <Zoom>
+        <Fade left>
           <section className="container">
             <section className="image">
               <img src={SelfCareBear} className="preview" />
@@ -36,13 +42,13 @@ export default function Home() {
                   Self Care <strong>Bear</strong>.
                 </h1>
               </section>
-              <section className="description">
-                <p>I'm a software developer from reno, nv.</p>
+              <section className="example-desc">
+                <p>self care bear is</p>
               </section>
             </section>
           </section>
-        </Zoom>
-        <Zoom>
+        </Fade>
+        <Fade left>
           <section className="container">
             <section className="image">
               <img src={DBD} className="preview" />
@@ -53,13 +59,13 @@ export default function Home() {
                   DBD <strong>Stats</strong>.
                 </h1>
               </section>
-              <section className="description">
-                <p>I'm a software developer from reno, nv.</p>
+              <section className="example-desc">
+                <p>DBD stats manager is </p>
               </section>
             </section>
           </section>
-        </Zoom>
-        <Zoom>
+        </Fade>
+        <Fade left>
           <section className="container">
             <section className="image">
               <img src={GotchiBB} className="preview" />
@@ -70,12 +76,12 @@ export default function Home() {
                   Gotchi <strong>BB</strong>.
                 </h1>
               </section>
-              <section className="description">
-                <p>I'm a software developer from reno, nv.</p>
+              <section className="example-desc">
+                <p>gotchi bb is</p>
               </section>
             </section>
           </section>
-        </Zoom>
+        </Fade>
       </main>
     </FadeIn>
   );
