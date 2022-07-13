8d43889fe1598e0d9d38496188b34bb35605713e
Ian Christiansen <ianc.songdog@gmail.com>
2022-07-13 10:54:31 -0700

    added tech stack

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -74,13 +74,65 @@ export default function Home() {
             <Fade>
               <section className="header">
                 <h2>
+                  tech <strong>stack</strong>.
+                </h2>
+              </section>
+              <section className="description">
+                <p>
+                  <b>languages:</b> Javascript. CSS3. HTML5.
+                  <b> Libraries + Frameworks:</b> React. Node.js. Express.js.
+                  Socket.IO. <b>Databases: </b> Supabase. PostgreSQL.
+                  <b>Testing: </b> Jest. QUnit. Supertest. <b>Dev Tools: </b>
+                  Git/Github. VSCode. Postman. Netlify. Heroku. <b>Design: </b>
+                  Adobe Suite.
+                </p>
+              </section>
+              <section className="header">
+                <h2>
                   recent <strong>projects</strong>.
                 </h2>
               </section>
             </Fade>
           </section>
         </section>
+        <Fade>
+          <section className="container">
+            <section>
+              <section className="example">
+                <h1>
+                  Gotchi <strong>BB</strong>.
+                </h1>
+              </section>
+              <section className="links">
+                <a
+                  href="https://github.com/Tamagotchi-Clone"
+                  target="_blank"
+                  alt="github repo"
+                >
+                  [github]
+                </a>
 
+                <a
+                  href="https://gotchi-bb.netlify.app"
+                  target="_blank"
+                  alt="deployed site"
+                >
+                  [deploy]
+                </a>
+              </section>
+              <section className="example-desc">
+                <p>
+                  <img src={GotchiBB} className="preview" />A full-stack CRUD
+                  app with a RESTful API that allows users to care for a virtual
+                  pet. Written on a 10-day sprint with a team of 4, I took the
+                  lead in project management, executed complex time-tracking
+                  logic, and personally implemented a natural language
+                  processing bot to allow users to chat with their pet.
+                </p>
+              </section>
+            </section>
+          </section>
+        </Fade>
         <Fade>
           <section className="container">
             <section>
@@ -92,6 +144,7 @@ export default function Home() {
               <section className="links">
                 <a
                   href="https://github.com/self-care-bear/self-care-bear"
+                  target="_blank"
                   alt="github repo"
                 >
                   [github]
@@ -99,6 +152,7 @@ export default function Home() {
 
                 <a
                   href="https://self-care-bears.netlify.app/"
+                  target="_blank"
                   alt="deployed site"
                 >
                   [deploy]
@@ -124,38 +178,31 @@ export default function Home() {
                   DBD <strong>Stats</strong>.
                 </h1>
               </section>
-              <section className="example-desc">
-                <img src={DBD} className="preview" />
-                <p>DBD stats manager is </p>
-              </section>
-            </section>
-          </section>
-        </Fade>
-        <Fade>
-          <section className="container">
-            <section>
-              <section className="example">
-                <h1>
-                  Gotchi <strong>BB</strong>.
-                </h1>
-              </section>
               <section className="links">
-                <a href="https://github.com/Tamagotchi-Clone" alt="github repo">
+                <a
+                  href="https://github.com/ian-ch-jsx/dbd-stats-manager"
+                  target="_blank"
+                  alt="github repo"
+                >
                   [github]
                 </a>
 
-                <a href="https://gotchi-bb.netlify.app" alt="deployed site">
+                <a
+                  href="https://dbd-stats-manager.netlify.app/"
+                  target="_blank"
+                  alt="deployed site"
+                >
                   [deploy]
                 </a>
               </section>
               <section className="example-desc">
                 <p>
-                  <img src={GotchiBB} className="preview" />A full-stack CRUD
-                  app with a RESTful API that allows users to care for a virtual
-                  pet. Written on a 10-day sprint with a team of 4, I took the
-                  lead in project management, executed complex time-tracking
-                  logic, and personally implemented a natural language
-                  processing bot to allow users to chat with their pet.
+                  {' '}
+                  <img src={DBD} className="preview" />A win/loss statistics
+                  manager for the game Dead By Daylight written in React and
+                  utilizing Supabase for the backend. Allows users to track
+                  win/loss statistics based on perk builds. Users can randomize
+                  their builds or manually input their perks as desired.
                 </p>
               </section>
             </section>
